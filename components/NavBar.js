7038ada85c1b2c331f0598f025d151b4d8bfb749
henryGustavo1514 <gustavo_1514@outlook.es>
2021-01-13 16:57:20 -0500

    link de descarga cv

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,120 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import $ from "jquery";
-import { Drawer, Button, Radio, Space } from 'antd';
+import React, { useEffect, useState } from 'react'
+import $ from 'jquery'
+import { Drawer, Button, Radio, Space } from 'antd'
 
 const NavBar = () => {
-    const [visible, setvisible] = useState(false)
+  const [visible, setvisible] = useState(false)
 
-
-    const [flag, setflag] = useState('stycky')
-    const listNav = [{
-        code: '.container-about',
-        name: 'Acerca de Mi',
-
+  const [flag, setflag] = useState('stycky')
+  const listNav = [
+    {
+      code: '.container-about',
+      name: 'Acerca de Mi'
     },
     {
-        code: '.container-trabajos ',
-        name: 'Trabajos',
-
-    }, {
-        code: 3,
-        name: 'Otros Proyectos',
-
+      code: '.container-trabajos ',
+      name: 'Trabajos'
     },
     {
-        code: 4,
-        name: 'Contacto',
-
+      code: 3,
+      name: 'Otros Proyectos'
     },
-    ]
-
-    const handleClickNav = (code) => {
-        const jumbotron = document.querySelector(`${code}`);
-        jumbotron.scrollIntoView({ behavior: "smooth" });
+    {
+      code: 4,
+      name: 'Contacto'
     }
+  ]
 
+  const handleClickNav = code => {
+    const jumbotron = document.querySelector(`${code}`)
+    jumbotron.scrollIntoView({ behavior: 'smooth' })
+  }
 
-    const showDrawer = () => {
-        setvisible(true)
-    };
+  const showDrawer = () => {
+    setvisible(true)
+  }
 
-    const onClose = () => {
-        setvisible(false)
+  const onClose = () => {
+    setvisible(false)
+  }
 
-    };
+  useEffect(() => {
+    handleClickNav('.container-infoprincipal')
 
-    useEffect(() => {
+    let scroll = $(document).scrollTop()
+    let navHeight = $('.container-navbar').outerHeight()
 
+    $(window).scroll(function () {
+      // console.log(scroll);
+      let scrolled = $(document).scrollTop()
+      if (scrolled > navHeight) {
+        $('.container-navbar').addClass('animate')
+      } else {
+        $('.container-navbar').removeClass('animate')
+      }
+      if (scrolled > scroll) {
+        $('.container-navbar').removeClass('stycky')
+      } else {
+        $('.container-navbar').addClass('stycky')
+      }
+      scroll = $(document).scrollTop()
+    })
+  }, [])
 
-        handleClickNav('.container-infoprincipal')
+  return (
+    <div className={`container-navbar ${flag}`} id='container-navbar'>
+      <h1>Henry Tipantuña</h1>
 
-        let scroll = $(document).scrollTop()
-        let navHeight = $('.container-navbar').outerHeight()
+      <div className='nav-items'>
+        <nav>
+          <ul>
+            {listNav.map((item, index) => (
+              <li key={index} onClick={() => handleClickNav(item.code)}>
+                {item.name}
+              </li>
+            ))}
 
-        $(window).scroll(function () {
-            // console.log(scroll);
-            let scrolled = $(document).scrollTop()
-            if (scrolled > navHeight) {
-                $('.container-navbar').addClass('animate')
+            <a href='files/HojaVida_HenryTipantuna.pdf' download>
+              Hoja de Vida
+            </a>
+          </ul>
+        </nav>
+      </div>
 
-            } else {
-                $('.container-navbar').removeClass('animate')
+      <div className='menu-hamburguesa'>
+        <button className='btn-icon-hamburguesa ' onClick={showDrawer}>
+          <i className='fas fa-bars'></i>
+        </button>
+      </div>
 
-            }
-            if ((scrolled > scroll)) {
-                $('.container-navbar').removeClass('stycky')
-            } else {
-                $('.container-navbar').addClass('stycky')
-
-            }
-            scroll = $(document).scrollTop()
-        })
-    }, [])
-
-
-    return (
-
-        <div className={`container-navbar ${flag}`} id='container-navbar'>
-
-            <h1>Henry Tipantuña</h1>
-
-            <div className='nav-items'>
-                <nav>
-                    <ul>
-                        {listNav.map((item, index) => (
-                            <li key={index} onClick={() => handleClickNav(item.code)}>{item.name}</li>
-                        ))}
-
-
-                        <button >Hoja de Vida</button>
-                    </ul>
-                </nav>
-            </div>
-
-            <div className='menu-hamburguesa'>
-
-                <button className='btn-icon-hamburguesa ' onClick={showDrawer}>
-
-                    <i className="fas fa-bars"></i>
-                </button>
-
-            </div>
-
-            <Drawer
-                title="Basic Drawer"
-                placement={'left'}
-                closable={false}
-                onClose={onClose}
-                visible={visible}
-                key={'left'}
-            >
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-            </Drawer>
-
-        </div>
-    );
+      <Drawer
+        title='Basic Drawer'
+        placement={'left'}
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+        key={'left'}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </div>
+  )
 }
 
-export default NavBar;
+export default NavBar
