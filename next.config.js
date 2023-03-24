0436f3e48b12dbd6b8bca604ee5cc861916db783
henryGustav <henry_gustavo18@hotmail.com>
2023-03-24 12:49:46 -0500

    adding basePath and assetPrefix

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+    basePath: "/nextjs-pages",
+    assetPrefix: "/nextjs-pages"
+  }
+  
+  module.exports = nextConfig
