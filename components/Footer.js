4e54aa440ed06b390a2462eaaa6881eb1b847b87
henryGustav <henr_gustavo18@hotmail.com>
2023-11-28 09:40:01 -0500

    update year fotter

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='footer section-padding '>
       <p className= 'text-center text-white'>Diseño y Construcción por Henry Tipantuña</p>
-      <p className='text-center text-white'>&copy; Henry Tipantuña 2021</p>
+      <p className='text-center text-white'>&copy; Henry Tipantuña 2023</p>
 
 
       {/* <h5 className= 'text-white'>Footer</h5> */}
